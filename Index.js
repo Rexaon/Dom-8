ff9c2405e2a342c5a6762427e663e411ffa00563
Rexaon <93644372+Rexaon@users.noreply.github.com>
2022-05-11 10:27:42 +0200

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,11 @@
+function firma(firmaNavn, antallAnsatte, bilListe, betaltSkatt, nettside, funksjon) {
+	firmaNavn = Tesla;
+	antallAnsatte = 200;
+	bilListe = Model_S, 3, X;
+
+    var eier = Eier(
+    "Emil Bronder", "Gutt", 17, "Skatt betalt", 100.000, "www.tesla.com", true
+);
+}
+
+console.log(firma);
